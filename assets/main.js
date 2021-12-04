bd9ee8404344079d309200a0e3a0a325baf05079
tzilovic <91481896+tzilovic@users.noreply.github.com>
2021-12-03 21:27:42 -0500

    added files (again)
    edited per wave accessibility

---
+++ assets/main.js
@@ -0,0 +1,113 @@
+/* FUNCTIONS FOR GRADIENT MAPPING */
+
+
+function originalgradient(){
+
+document.getElementById("gradient-mapping-img").src = "assets/gradient-mapping/gradient-mapping_original.png";
+
+}
+
+
+function bluegradient(){
+
+document.getElementById("gradient-mapping-img").src = "assets/gradient-mapping/gradient-mapping_blue.png";
+
+}
+
+function purplegradient(){
+
+document.getElementById("gradient-mapping-img").src = "assets/gradient-mapping/gradient-mapping_purple.png";
+
+}
+
+function pinkgradient(){
+
+document.getElementById("gradient-mapping-img").src = "assets/gradient-mapping/gradient-mapping_pink.png";
+
+}
+
+
+/* FUNCTIONS FOR DOUBLE EXPOSURE */
+
+
+function imgone(){
+
+document.getElementById("double-exposure-img").src = "assets/double-exposure/double-exposure_one.png";
+
+}
+
+
+function imgtwo(){
+
+document.getElementById("double-exposure-img").src = "assets/double-exposure/double-exposure_two.png";
+
+}
+
+
+
+function imgblended(){
+
+document.getElementById("double-exposure-img").src = "assets/double-exposure/double-exposure_final.png";
+
+}
+
+
+
+/* FUNCTIONS FOR MASKING*/
+
+
+function maskone(){
+
+document.getElementById("masking-region").src = "assets/masking-region/masking-region_one.png";
+
+}
+
+
+function masktwo(){
+
+document.getElementById("masking-region").src = "assets/masking-region/masking-region_two.png";
+
+}
+
+
+function maskthree(){
+
+document.getElementById("masking-region").src = "assets/masking-region/masking-region_mask.png";
+
+}
+
+
+function maskblend(){
+
+document.getElementById("masking-region").src = "assets/masking-region/masking-region_final.png";
+
+}
+
+
+
+/* FUNCTIONS FOR COLOR GRADING */
+
+function gradingoriginal(){
+
+document.getElementById("color-grading-img").src = "assets/color-grading/color-grading_original.png";
+
+}
+
+function gradingone(){
+
+document.getElementById("color-grading-img").src = "assets/color-grading/color-grading_curves-one.png";
+
+}
+
+function gradingtwo(){
+
+document.getElementById("color-grading-img").src = "assets/color-grading/color-grading_curves-two.png";
+
+}
+
+function gradingthree(){
+
+document.getElementById("color-grading-img").src = "assets/color-grading/color-grading_curves-three.png";
+
+}
+
